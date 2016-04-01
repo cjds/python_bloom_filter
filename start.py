f5b5f5208c78f1caf554152d11f6cdac20982168
Carl Saldanha <cjds@lawn-128-61-19-146.lawn.gatech.edu>
2016-03-31 21:25:14 -0400

    updated plot algorithms

--- start.py
+++ start.py
@@ -7,75 +7,78 @@ class BloomFilter:
 
 	def __init__(self,seed,n,k):
 		random.seed(seed)
-		self.random=random.randint(0,seed)
-		self.n=n
+		self.rand=random.randint(0,n)
+		self.n=n 
 		self.filter=[0]*n
 		self.k=k
 
-	def reseed(self):
+	def reseed(self,seed):
 		random.seed(seed)
 
-	def hashInput(self,input):
+	def choose_digits(self,datum):		
+		random.seed(datum * self.rand)
 		output=[]
-		random.seed(input)
-		current_filter=[0]*self.n
-		for i in range(int(self.k)):
-			#no_of_digits=n-(k*digits) if (i==k) else digits
-			hashout=random.randint(0,self.n-1)
-			current_filter[hashout] = 1
-			
-		return current_filter
+		for i in xrange(int(self.k)):
+			output.append(random.randint(0,self.n-1))
+		return output
 
-	def add_to_bloom(self,hashout):
-		self.filter= [(1 if (x ==1 or y==1) else 0 )for x, y in zip(self.filter, hashout)]
+	def hashInput(self,input):
+		digits=self.choose_digits(input)
+		for digit in digits:
+			#no_of_digits=n-(k*digits) if (i==k) else digits
+			self.filter[digit] = 1
+		return self.filter
+
+	#def add_to_bloom(self,hashout):
+		#self.filter= [(1 if (x ==1 or y==1) else 0 )for x, y in zip(self.filter, hashout)]
 
 	def test_filter(self,test_number):
-		for i,digit in enumerate(self.filter):
-			if(test_number[i]==1 and not digit==1):
+		digits=self.choose_digits(test_number)
+		for digit in digits:
+			if(not self.filter[digit] == 1):
 				return False
 		return True
-
 
 def main():
 	seed=123
 
-	start_n=5500
-	end_n=6000
+	start_n=100
+	end_n=10000
 
-	start_m =1000
-	end_m= 1200
+	start_m =100
+	end_m= 2000
 
 	step=50
-	N = int(math.pow(2,12))-1
+	N = int(math.pow(2,20))-1
 
-	collisions= [[0 for x in range(0,end_n-start_n,step)] for x in range(0,end_m-start_m,step)] 
-
+	collisions= [[0.0 for x in range(0,end_n-start_n,step)] for x in range(0,end_m-start_m,step)] 
 	k=1
 
-	for n in range(start_n,end_n,step): #table size
+	for n in range(start_n,end_n,step): #table 
+		print n
 		for m in range(start_m,end_m,step): # number of entries being inserted
 			c= n / m
 			k = int(math.ceil(c * 0.69314718056)) #ln 2
+			#print "K: %d"% k
 			bloom_filter=BloomFilter(seed,n,k)
-			for b in xrange(0,m):
-				v=random.randint(0,N) #choose randomly from the size of universe
-				hashedv=bloom_filter.hashInput(v)
-				while not bloom_filter.test_filter(hashedv):
-					v=random.randint(0,N)
-				bloom_filter.add_to_bloom(hashedv)
+			values_to_insert=random.sample(range(0, N), m)
+			for v in values_to_insert:
+				bloom_filter.hashInput(v)
 
 			#test all the numbers
 			for t in range(0,N):
-				if(bloom_filter.test_filter(bloom_filter.hashInput(t))):
-					collisions[(m-start_m)/step-1][(n-start_n)/step]+= 1
+				if(bloom_filter.test_filter(t)):
+					collisions[(m-start_m)/step][(n-start_n)/step]+= 1.0
 
-			collisions[(m-start_m)/step-1][(n-start_n)/step]/= float(N)
-			collisions[(m-start_m)/step-1][(n-start_n)/step ]*= float(100)
-	plot(range(start_n,end_n,step),collisions[1])
+			collisions[(m-start_m)/step][(n-start_n)/step]/= float(N)
+			collisions[(m-start_m)/step][(n-start_n)/step ]*= float(100)
+		print "Collisions %f" % (collisions[(m-start_m)/step][(n-start_n)/step ])
+	save_file('data.json',collisions)
+	plot(range(start_n,end_n,step),collisions[0])
 
 def save_file(file_name,data):
-	with open('data.json', 'w') as outfile:
-   		json.dump(collisions, outfile)
+	with open(file_name, 'w') as outfile:
+   		json.dump(data, outfile)
 
 def plot(x,y):
 	plt.xlabel('n')
